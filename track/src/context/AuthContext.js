c2870e51c983c623ce24a7175d5404b6bae0396e
Sungmin <spark@Sungmins-MacBook-Pro.local>
2019-12-24 00:00:38 +0900

    edit AuthContext.js

--- track/src/context/AuthContext.js
+++ track/src/context/AuthContext.js
@@ -7,8 +7,40 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+
+    // if we sign up, modify our state, and say authenticated
+
+    // if sign up fails, need to reflect an error message to user
+  }
+
+}
+
+const login = (dispatch) => {
+  return ({ email, password }) => {
+    // try to login
+
+    // handle success by updating state
+
+    // handle failure by showing error message
+  }
+}
+
+const logout = (dispatch) => {
+  return ({ email, password }) => {
+    // try to login
+
+    // handle success by updating state
+
+    // handle failure by showing error message
+  }
+}
+
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signup, login, logout },
   { isSignedIn: false }
 );
